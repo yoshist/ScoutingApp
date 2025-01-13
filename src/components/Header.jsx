b362c855fce18cdf3161660a638daa031d9c015a
yoshist <stacy.yoshimitsu@gmail.com>
2025-01-12 18:22:47 -0800

    Applied styles to the header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
+import './Header.css';
 
 export default function Header() {
   return (
-    <div>
-        <header>
-        <div>
-            <ul id="scouting-apps">
+     <div className="outer-div-style">
+        <header className="header-style flex">
+        <div className="scouting-apps">
+          <h3>Scouting Apps</h3>
+          <hr />
+            <ul className="menu flex">
                 <li>Match</li>
                 <li>Strategic</li>
                 <li>Pit</li>
             </ul>
         </div>
-        <div>
-          <ul id="scouting-reports">
+        <div className="scouting-reports">
+          <h3>Reports</h3>
+          <hr />
+          <ul className="menu flex">
               <li>DTDF</li>
-              <li>Team Score</li>
+              <li>Team Score</li>         
           </ul>
         </div>
         </header>
-    </div>
+     </div>
   )
 }
