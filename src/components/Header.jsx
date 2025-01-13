e5deb7dad224892164b73b13618068960fa97190
yoshist <stacy.yoshimitsu@gmail.com>
2025-01-12 20:28:29 -0800

    Updated header style.  Replaced header menu item with buttons.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,51 @@
 import React from 'react'
 import './Header.css';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  
+  const location = useLocation()
+  const navigate = useNavigate();
+  console.log(location.pathname);
+
+  function pathMatchRoute(route) {
+    if(route === location.pathname){
+      return true;
+    }
+  }
+
   return (
      <div className="outer-div-style">
         <header className="header-style flex">
         <div className="scouting-apps">
           <h3>Scouting Apps</h3>
-          <hr />
-            <ul className="menu flex">
-                <li>Match</li>
-                <li>Strategic</li>
-                <li>Pit</li>
-            </ul>
+
+            <div className="menu flex">            
+                <div className={`menu-item font-weight: 900; ${
+                  pathMatchRoute("/match-app") ? "current-page-app" : ""}`} 
+                  onClick={() => navigate("/match-app")}><div>Match</div></div>
+
+                <div className={`menu-item font-weight: 900; ${
+                  pathMatchRoute("/strategic-app") ? "current-page-app" : ""}`} 
+                  onClick={() => navigate("/strategic-app")}><div>Strategic</div></div>
+
+                <div className={`menu-item font-weight: 900; ${
+                  pathMatchRoute("/pit-app") ? "current-page-app" : ""}`} 
+                  onClick={() => navigate("/pit-app")}><div>PIT</div></div>
+            </div>
         </div>
         <div className="scouting-reports">
           <h3>Reports</h3>
-          <hr />
-          <ul className="menu flex">
-              <li>DTDF</li>
-              <li>Team Score</li>         
-          </ul>
+
+          <div className="menu flex">
+              <div className={`menu-item font-weight: 900; ${
+                  pathMatchRoute("/dtf-report") ? "current-page-report" : ""}`} 
+                  onClick={() => navigate("/dtf-report")}><div>DTF</div></div>
+
+              <div className={`menu-item font-weight: 900; ${
+              pathMatchRoute("/team-score-report") ? "current-page-report" : ""}`} 
+              onClick={() => navigate("/team-score-report")}><div>Team Score</div></div>        
+          </div>
         </div>
         </header>
      </div>
